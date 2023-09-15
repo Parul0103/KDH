34eca2b6a292b39f0959fe0956c15ebf645a3ddd
Parul0103 <134639170+Parul0103@users.noreply.github.com>
2023-09-15 23:03:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,9 @@ const products = require('./routes/product')
 const auth = require('./routes/auth')
 const order = require('./routes/order')
 
-app.use('/api/v1',products)
-app.use('/api/v1',auth)
-app.use('/api/v1',order)
+app.use('/api/v1/',products)
+app.use('/api/v1/',auth)
+app.use('/api/v1/',order)
 
 if(process.env.NODE_ENV !== 'PRODUCTION') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
